0d29ba56e16f1f33cc3bf8e3260d9c3a303f7260
roxm <rossymr@gmail.com>
2015-08-13 12:31:21 -0500

    Refactor newlyCreatedStack_shouldbeEmpty
    added size

--- spec/stackKata_spec.rb
+++ spec/stackKata_spec.rb
@@ -2,9 +2,10 @@ require_relative "../stackKata"
 
 describe MyStack do
 
-it 'createEmptyStack' do
+it 'newlyCreatedStack_shouldbeEmpty' do
 	myStack = MyStack.new
 	expect(myStack.isEmpty).to be_truthy
+	expect(myStack.getSize).to eq 0	
 end
 
 end
--- stackKata.rb
+++ stackKata.rb
@@ -1,7 +1,15 @@
 class MyStack
+def initialize
+	@empty = true
+	@size = 0
+end
 
 def isEmpty
-	true
+	@empty
+end
+
+def getSize
+	@size
 end
 
 
